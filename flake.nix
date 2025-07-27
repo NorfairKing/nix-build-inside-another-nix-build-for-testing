b43f30cc22b2c631e4e9cb697ea154c54564332e
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-07-27 11:57:09 +0200

    trying a thing

--- flake.nix
+++ flake.nix
@@ -13,6 +13,26 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+
+      # makePureImpure : Derivation -> Derivation
+      # Copied from https://github.com/NorfairKing/pure-impure-nix
+      # See https://github.com/NorfairKing/pure-impure-nix/blob/0941bd8b379827fb0341341f999dd27fd615328e/flake.nix#L17-L55 for an explanation.
+      makePureImpure = drv: drv.overrideAttrs (old:
+        let
+          magicString = builtins.unsafeDiscardStringContext (builtins.substring 0 12 (baseNameOf drv.drvPath));
+          outputHashAlgo = "sha256";
+          outputHash = builtins.hashString outputHashAlgo magicString;
+        in
+        {
+          preferHashedMirrors = false;
+          inherit outputHashAlgo;
+          inherit outputHash;
+          buildCommand = ''
+            ${old.buildCommand or ""}
+            rm -rf $out
+            echo -n "${magicString}" > $out
+          '';
+        });
     in
     {
       checks.${system} = {
@@ -32,6 +52,16 @@
           export NIX_LOG_DIR=$(mktemp -d)
           nix build ${./simple}# > $out
         '';
+        build-this-shell = makePureImpure (pkgs.runCommand "nix-build-this-shell" { buildInputs = [ pkgs.nix pkgs.cacert ]; } ''
+          sandboxdir=$(mktemp -d)
+          export NIX_CONFIG=$'experimental-features = nix-command flakes\nsandbox-build-dir = '"$sandboxdir"
+          export NIX_STATE_DIR=$(mktemp -d)
+          export NIX_LOG_DIR=$(mktemp -d)
+          export NIX_STORE_DIR=$(mktemp -d)
+          export HOME=$(mktemp -d)
+          nix build "${self}#devShells.${system}.default"
+          touch $out
+        '');
         pre-commit = pre-commit-hooks.lib.${system}.run {
           src = ./.;
           hooks = {
